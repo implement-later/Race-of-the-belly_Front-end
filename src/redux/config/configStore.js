815747764f1f86b80bbfb0774a07833c04a0d0f7
MildColor <96479626+MildColor@users.noreply.github.com>
2022-10-25 01:35:40 +0900

    error 수정

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import restaurant from "../modules/restaurantSlice";
 import user from "../modules/login/loginSlice";
 import orderdetail from "../modules/orderlistSlice";
 import menulist from "../modules/menulistSlice";
-import restaurantlist from "../modules/restaurantlistSlice/restaurantSlice";
 import menu from "../modules/menuSlice";
-
+import restaurantlist from "../modules/restaurantlistSlice/restaurantSlice";
 const store = configureStore({
-  reducer: { restaurant, user, menulist, orderdetail, menu, restaurantlist },
+  reducer: { user, menulist, orderdetail, menu, restaurantlist },
 });
 export default store;
